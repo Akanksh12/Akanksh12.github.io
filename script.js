f4ae595a91c5aeaca96fec34e249569450f68459
Akanksh Chitimalla <55909985+Akanksh12@users.noreply.github.com>
2024-05-22 14:04:08 +0530

    added a script to trigger particles and removed default text

---
+++ script.js
@@ -0,0 +1,28 @@
+/*
+particles: {
+  color: {
+    value: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"],
+    },
+  }
+ */
+
+function particles() {
+
+  tsParticles.load({
+    id: "tsparticles",
+    options: {
+      particles: {
+        color: {
+          value: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"]
+        },
+      },
+      emitters: {
+        life: {
+          duration: 0,
+        },
+      },
+      preset: "confetti",
+    },
+  });
+
+}
