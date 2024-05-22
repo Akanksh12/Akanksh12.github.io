a3f57c343db0d112d58a9e5e2d36b41dfbb105d4
Akanksh Chitimalla <55909985+Akanksh12@users.noreply.github.com>
2024-05-22 19:02:00 +0530

    changed to +1 hour time offset because it's british summer time zone currently

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ particles: {
 
 /*Date('May 23, 2024 00:00:00 GMT+00:00').getTime()*/
 let date = new Date()
-if (date.getTime() < new Date('May 23, 2024 00:00:00 GMT+00:00').getTime()){
+if (date.getTime() < new Date('May 23, 2024 00:00:00 GMT+01:00').getTime()){
   document.body.style.display = "none"
 }
 
